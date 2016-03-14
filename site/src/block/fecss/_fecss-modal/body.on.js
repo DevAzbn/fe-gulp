6f6dd45d2a9bbd5e346a6141dc4905269243b0bc
Alexander <alexander.zybin@gmail.com>
2016-03-14 12:18:37 +0300

    201603141218/work: Patch and change fecss-modal

--- site/src/block/fecss/_fecss-modal/body.on.js
+++ site/src/block/fecss/_fecss-modal/body.on.js
@@ -43,19 +43,26 @@ $(document.body).on('click.fecss.modal.show', '.fecss-modal-btn', {}, function(e
 	var btn = $(this);
 	var href = btn.attr('href');
 	
-	var wc = $(document.body).find('.fecss-modal ' + href + '.white-container')
+	var wc = $(document.body).find('.fecss-modal ' + href + '.white-container');
+	var modal = wc.closest('.fecss-modal');
 	
 	if(wc.size()) {
 		
-		var modal = wc.closest('.fecss-modal');
+		var _prev = $(document.body).find('.fecss-modal .white-container.active:not(.in-bg)').eq(0);
+		if(_prev.size()) {
+			_prev.addClass('in-bg');
+			_prev.closest('.fecss-modal').addClass('in-bg');
+			wc.data('fecss-modal-prev', _prev.attr('id'));
+		}
+		
 		if(!modal.hasClass('active')) {
-			modal.addClass('active');
+			modal.addClass('active').removeClass('in-bg');
 		}
 		
 		var bc = modal.find('.black-container');
 		bc.css({top : $(document).scrollTop() + 50 + 'px',});
 		
-		wc.addClass('active').trigger('fecss.active.set');
+		wc.addClass('active').removeClass('in-bg').trigger('fecss.active.set');
 		
 		$(document.body).trigger('fecss.modal.show.after', [modal, wc]);
 		
@@ -71,10 +78,33 @@ $(document.body).on('click.fecss.modal.hide', '.fecss-modal .white-container .hi
 	var wc = btn.closest('.white-container');
 	var modal = btn.closest('.fecss-modal');
 	
-	if(modal.find('.white-container.active').size()) {
+	if(wc.size()) {
 		
-		modal.removeClass('active');
-		wc.removeClass('active').trigger('fecss.active.unset');
+		wc.removeClass('active').removeClass('in-bg');
+		
+		var _prev = $('#' + wc.data('fecss-modal-prev'));
+		if(_prev.size()) {
+			
+			var _prev_modal = _prev.closest('.fecss-modal');
+			
+			if(_prev_modal.hasClass('active')) {
+				if(!_prev_modal.hasClass('in-bg')) {
+					
+				} else {
+					_prev_modal.removeClass('in-bg');
+				}
+			} else {
+				modal.removeClass('active');
+				_prev_modal.addClass('active').removeClass('in-bg');
+			}
+			
+			_prev.removeClass('in-bg');
+			
+		} else {
+			modal.removeClass('active');
+		}
+		
+		wc.data('fecss-modal-prev', '').trigger('fecss.active.unset');
 		
 		$(document.body).trigger('fecss.modal.hide.after', [modal, wc]);
 		
