9bffd0a8a81b0c284a6ea520f191b55ee84d135f
Alexander <alexander.zybin@gmail.com>
2016-03-19 00:42:26 +0300

    201603190042/home: Add fecss-imgresizer

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ var gulp = require('gulp'),
 	plumber = require('gulp-plumber'),					// отлов ошибок
 	pagebuilder = require('gulp-pagebuilder');			// умный инклуд html с поддержкой вложенности и передачей параметров
 
-var root = 'pf5',//'site',//'rybka',
+var root = 'site',//'site',//'rybka',
 	src = root + '/' + 'src';
 
 var path = {
