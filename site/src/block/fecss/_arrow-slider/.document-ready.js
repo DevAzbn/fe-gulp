f5f44b1c250cd822bfaa8b2b11e80061cf1f76c0
Alexander <alexander.zybin@gmail.com>
2016-06-03 10:00:17 +0300

    201606031000/work: Change arrow-slider

--- site/src/block/fecss/_arrow-slider/.document-ready.js
+++ site/src/block/fecss/_arrow-slider/.document-ready.js
@@ -10,31 +10,56 @@
 		//var counter = block.find('.info-block .counter');
 		//var position = counter.find('.position');
 		
-		imgs.each(function(index){
-			//$(this).attr('data-index', index+1);
-			$('<a/>',{
-				class : 'item',
-				html : '<span class="point" ></span>',
-				href : '#image-' + index,
-			})
-			.on('click.arrow-slider.point',function(event){
-				console.log('click.arrow-slider.point');
-				var i = $(this).index();
-				point_line.find('.item').removeClass('active');
-				imgs.fadeOut('fast').removeClass('active');
-				$(this).addClass('active');
-				imgs.eq(i).fadeIn('fast').addClass('active');
-			})
-			.appendTo(point_line);
-		});
-		/*
-		imgs.eq(0).each(function(index){
-			$(this).addClass('active');
-			//counter.find('.position').html(index+1);
-			//counter.find('.form').html(imgs.size());
-		});
-		*/
-		arrow_block.on('click.arrow-slider.right', '.btn-arrow.right', function(event){
+		block.on('init', function(event){
+			event.preventDefault();
+			
+			console.log('.arrow-slider init');
+			
+			var block_h = block.attr('data-block-height') || 480;
+			
+			block.css({
+				height : block_h + 'px',
+			});
+			
+			point_line.empty();
+			
+			imgs.each(function(index){
+				//$(this).attr('data-index', index+1);
+				$('<a/>',{
+					class : 'item',
+					html : '<span class="point" ></span>',
+					href : '#image-' + index,
+				})
+				.on('click.fecss.arrow-slider.point',function(event){
+					console.log('click.fecss.arrow-slider.point');
+					var i = $(this).index();
+					point_line.find('.item').removeClass('active');
+					imgs.fadeOut('fast').removeClass('active');
+					$(this).addClass('active');
+					imgs.eq(i).fadeIn('fast').addClass('active');
+				})
+				.appendTo(point_line);
+			});
+			
+			if(block.hasClass('with-timer')) {
+				block.data('fecss-timer', setInterval(function(){
+					if(block.is(':hover')) {
+						
+					} else {
+						arrow_block.find('.btn-arrow.right').trigger('click');
+					}
+				}, 3000))
+			}
+			
+			if(point_line.find('.item.active').size()) {
+				
+			} else {
+				point_line.find('.item').eq(0).trigger('click');
+			}
+			
+		}).trigger('init');
+		
+		block.on('click.fecss.arrow-slider.right', '.btn-arrow.right', function(event){
 			var p = point_line.find('.item');
 			var i = p.filter('.active').eq(0).index();
 			var nxt = p.eq(i).next('.item');
@@ -44,7 +69,8 @@
 				p.eq(0).trigger('click');
 			}
 		});
-		arrow_block.on('click.arrow-slider.left', '.btn-arrow.left', function(event){
+		
+		block.on('click.fecss.arrow-slider.left', '.btn-arrow.left', function(event){
 			var p = point_line.find('.item');
 			var i = p.filter('.active').eq(0).index();
 			var nxt = p.eq(i).prev('.item');
@@ -54,22 +80,6 @@
 				p.eq(-1).trigger('click');
 			}
 		});
-		
-		if(block.hasClass('with-timer')) {
-			block.data('fecss-timer', setInterval(function(){
-				if(block.is(':hover')) {
-					
-				} else {
-					arrow_block.find('.btn-arrow.right').trigger('click');
-				}
-			}, 3000))
-		}
-		
-		if(point_line.find('.item.active').size()) {
-			
-		} else {
-			point_line.find('.item').eq(0).trigger('click');
-		}
 		
 	});
 	
